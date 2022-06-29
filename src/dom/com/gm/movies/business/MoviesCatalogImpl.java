76ca0618673b858d12093264b97da83f46cfd532
JhordP <j.horda.ny@hotmail.com>
2022-06-29 10:45:30 -0400

    Empty implementation of MovieCatalog and DataAccess

--- src/dom/com/gm/movies/business/MoviesCatalogImpl.java
+++ src/dom/com/gm/movies/business/MoviesCatalogImpl.java
@@ -1,5 +1,24 @@
 package dom.com.gm.movies.business;
 
+import dom.com.gm.movies.data.DataAccessImpl;
+
 public class MoviesCatalogImpl {
     
+    DataAccessImpl data;
+
+    public void addMovie(String movieName, String fileName){
+
+    }
+
+    public void movieList(String fileName){
+
+    }
+    
+    public void searchMovie(String fileName, String search){
+
+    }
+
+    public void startFile(String fileName){
+
+    }
 }
--- src/dom/com/gm/movies/data/DataAccessImpl.java
+++ src/dom/com/gm/movies/data/DataAccessImpl.java
@@ -1,5 +1,27 @@
 package dom.com.gm.movies.data;
 
 public class DataAccessImpl {
-    
+    public boolean exists(String fileName) {
+
+    }
+
+    public List<Movie> list(String name){
+        
+    }
+
+    public void write(Movie movie, String fileName, boolean attach){
+
+    }
+
+    public String search(String fileName, String search){
+
+    }
+
+    public void create(String fileName){
+
+    }
+
+    public void delete(String fileName){
+
+    }
 }
